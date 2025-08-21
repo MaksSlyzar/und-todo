e5464c03f4c1f089e5055bb88407156e539b8310
MaksSlyzar <maks.slyzar@gmail.com>
2025-08-21 23:25:45 +0300

    pixi js is bad...

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,1 @@
-import ObjectManager from "./ObjectManager";
-import "./style.css";
-import { Application } from 'pixi.js';
 
-(async () => {
-  const app = new Application();
-
-  await app.init({ background: '#1E1E1E', resizeTo: window });
-
-  document.body.appendChild(app.canvas);
-
-  const objectsManager = new ObjectManager(app);
-})();
